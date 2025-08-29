d615cdd5558ded41ddb2b0d54548646b3bab1fee
James Stine <james.stine@okstate.edu>
2025-08-29 00:36:15 -0500

    change riscv_pipelined2.sv

--- riscv_pipe/riscv_pipelined2.sv
+++ riscv_pipe/riscv_pipelined2.sv
@@ -102,7 +102,7 @@ module testbench();
    initial begin
       string memfilename;
       string dmemfilename;
-      memfilename = {"../testing/template.memfile"};
+      memfilename = {"../testing/riscvtestCSR.memfile"};
       $readmemh(memfilename, dut.imem.RAM);
       $readmemh(memfilename, dut.dmem.RAM);	
    end
